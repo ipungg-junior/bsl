b885e7d27d5c59d6362533465d0377e2cf742bf1
root <root@systema.id>
2024-02-26 13:43:22 +0700

    create models file and add templates for merchandise

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Notification(models.Model):
+    title = models.CharField(max_length=30, null=False)
+    content = models.CharField(max_length=100, null=False)
+    # to = models.ForeignKey(Department, on_delete=models.CASCADE)
--- merchandise/views.py
+++ merchandise/views.py
@@ -11,7 +11,9 @@ class SoC(View):
         pass
 
     def get(self, request):
-        return render(request, 'merchandise_soc.html')
+        system_info = True
+        system_info_content = 'Informasi penting untuk seluruh karyawan yang masih belum submit laporan harian tanggal 21, segera lakukan konfirmasi dengan Administrator.'
+        return render(request, 'merchandise_soc.html', context={'system_info': system_info, 'system_info_content': system_info_content})
     
 
 class Sample(View):
